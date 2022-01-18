3277629f75fa8c0ae8908613aecc5b01dee0c8c8
FarhanAzizi1998 <97634354+FarhanAzizi1998@users.noreply.github.com>
2022-01-18 11:32:22 +0100

    Add files via upload

---
+++ oppgave_a.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep  9 13:03:38 2021
+
+@author: Erlend Tøssebro
+"""
+
+hoyde_cm = float(input("Skriv inn høyde i cm: "))
+if hoyde_cm >= 120:
+    print("Denne kunden kan ta karusellen")
+else:
+    print("Denne kunden er for lav til å kunne ta karusellen")
+
